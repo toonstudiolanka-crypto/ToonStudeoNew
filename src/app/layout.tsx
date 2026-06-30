36257813833f0414a4488b79d3db8bc0bdaf70cd
DIshanlk <sldishankavinda@gmail.com>
2026-06-30 11:04:20 +0530

    neww

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,11 +24,13 @@ const caveat = Caveat({
 export const metadata: Metadata = {
   icons: {
     icon: [
-      { url: "/favicon.ico", sizes: "any" },
-      { url: "/favicon.png", type: "image/png", sizes: "32x32" },
+      { url: "/favicon/favicon.ico", sizes: "any" },
+      { url: "/favicon/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/favicon/favicon-32x32.png", sizes: "32x32", type: "image/png" },
     ],
-    apple: "/apple-touch-icon.png",
+    apple: "/favicon/apple-touch-icon.png",
   },
+  manifest: "/favicon/site.webmanifest",
 };
 
 export const viewport: Viewport = {
